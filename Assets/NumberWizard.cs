a06d5be2c02c4d02325344b86ef805ff902d51f4
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-24 23:17:25 +0000

    Initial commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Please pick a number.");
+        Debug.Log("The highest numeber you can chose is 100.");
+        Debug.Log("The lowest number you can chose is 1.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
