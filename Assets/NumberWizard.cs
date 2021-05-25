0a00945fd14ab036104ebdbaee8cae97904b9938
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-25 23:40:45 +0000

    Pedro adds highest and lowest number values stored in integer values.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,10 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to number wizard!");
         Debug.Log("Please pick a number.");
-        Debug.Log("The highest numeber you can chose is 100.");
-        Debug.Log("The lowest number you can chose is 1.");
+        Debug.Log("The highest numeber you can chose is: " + max);
+        Debug.Log("The lowest number you can chose is: " + min);
     }
 
     // Update is called once per frame
