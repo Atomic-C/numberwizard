b31e5909886c2d6f1bca9f95a1f306356bd62f2a
Pedro Pacheco de Sousa <pedro.p.sousa@sapo.pt>
2021-05-25 23:58:56 +0000

    Pedro adds first user input Up Arrow

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -14,11 +14,17 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Please pick a number.");
         Debug.Log("The highest numeber you can chose is: " + max);
         Debug.Log("The lowest number you can chose is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
     }
 }
